93f86255c7433387f124d02216117f35dd396b2f
dolarge <dohyun722@daum.net>
2023-12-03 22:49:32 +0900

    간단 설명

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+/*
+HTTP 메소드(get방식), 라우팅 ,
+app.get('/',()=>)
+
+라우팅
+/about
+/board
+/aboud/etc
+
+콜백(callback)
+()=>{}
+함수(끝나고 실행할 함수)
+다른 코드의 인수로서 넘겨주는 함수
+
+*/
