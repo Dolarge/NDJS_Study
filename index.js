580d0800747b803c2410623546901cd864624c3d
dolarge <dohyun722@daum.net>
2023-12-03 22:58:19 +0900

    route dog/cat

--- index.js
+++ index.js
@@ -2,25 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//기본값으로 라우팅되면 아래와 같이 표시하겠다.
+//res : 응답, 응답에 hello world를 표시하겠다.
+//req : 어떠한 값을 넘겨줄껀지.
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/dog', (req, res) => {
+    res.send('Hello Dog!')
+})
+
+app.get('/cat', (req, res) => {
+    res.send('Hello Cat!')
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-/*
-HTTP 메소드(get방식), 라우팅 ,
-app.get('/',()=>)
-
-라우팅
-/about
-/board
-/aboud/etc
-
-콜백(callback)
-()=>{}
-함수(끝나고 실행할 함수)
-다른 코드의 인수로서 넘겨주는 함수
-
-*/
