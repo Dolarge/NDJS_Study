f409d952e949297e6b438e1d54a342168701d54c
dolarge <dohyun722@daum.net>
2023-12-06 23:04:55 +0900

    get id

--- index.js
+++ index.js
@@ -10,11 +10,20 @@ app.get('/', (req, res) => {
 })
 
 app.get('/dog', (req, res) => {
-    res.send('Hello Dog!')
+    res.send({'사운드':'멍멍'})
 })
 
 app.get('/cat', (req, res) => {
     res.send('Hello Cat!')
+})
+
+//get 방식으로 쿼리또는 파라미터로 값을 전달해 줄 수 있다.
+//파라미터를 받을때는 앞에 : 붙여야함
+app.get('/user/:id', (req, res) => {
+  const q= req.params
+  console.log(q)
+
+  res.json({'userid':q.id})
 })
 
 
